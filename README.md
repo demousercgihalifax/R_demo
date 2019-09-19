4bd5db689dfaf64707e4b588a25766a59febf589
demousercgihalifax <55547764+demousercgihalifax@users.noreply.github.com>
2019-09-19 11:43:29 -0300

    First Commit from RStudio

---
+++ README.md
@@ -0,0 +1,2 @@
+# R_demo
+Demo code for Web Scraping using R
---
+++ connect to github.R
@@ -0,0 +1,6 @@
+# Reference Document
+# https://bren.zendesk.com/hc/en-us/articles/360015826731-How-to-connect-RStudio-Cloud-with-Github
+
+# run below on terminal 
+git config --global user.email "demo.user.cgi.halifax@gmail.com"
+git config --global user.name "demousercgihalifax"
