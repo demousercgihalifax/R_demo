be3f38b71f0ccf1e729ae88d004cc3b7b470cb5a
demousercgihalifax <55547764+demousercgihalifax@users.noreply.github.com>
2019-10-01 19:38:07 -0300

    Update call_scraper_function.R

--- web_scraping_using_r/code/call_scraper_function.R
+++ web_scraping_using_r/code/call_scraper_function.R
@@ -2,9 +2,9 @@ source('/cloud/project/web_scraping_using_r/code/scraper_function.R')
 
 for (i in 1:10)
 {
-  yahoo_fin('GIB-A.TO','ca')
+  yahoo_fin(script='AAPL' ,country='ca',from_email="demo.user.cgi.halifax@gmail.com",from_password="xxxxxxx",to_email="demo.user.cgi.halifax@gmail.com")
   Sys.sleep(360)
 }
 
-yahoo_fin('GIB-A.TO','ca') #CGI Inc. code
-yahoo_fin('ICICIBANK.BO','in')
+yahoo_fin(script='AAPL' ,country='ca',from_email="demo.user.cgi.halifax@gmail.com",from_password="xxxxxxxx",to_email="demo.user.cgi.halifax@gmail.com") #CGI Inc. code
+#yahoo_fin('ICICIBANK.BO','in')
